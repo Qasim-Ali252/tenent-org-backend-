07d342eb67cf0dc4e58518b209b7e1008e0c58f6
Qasim-Ali252 <qasimmunir633@gmail.com>
2026-02-17 19:15:38 +0500

    add authentication module index

---
+++ src/modules/auth/authentication/index.js
@@ -0,0 +1,6 @@
+import router from './router.js';
+import controller from './controller.js';
+import service from './service.js';
+
+export { router, controller, service };
+export default { router, controller, service };
